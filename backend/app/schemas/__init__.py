b64fe7a996a8d400bf9612fa4e98725c07ba6f34
Elmurod <elmurod20050402@gmail.com>
2026-07-01 16:03:47 +0500

    Delete backend directory
    delete

--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from .task import TaskCreate, TaskResponse, TaskListResponse
-from .user import UserInfo
-from .employee import EmployeeCreate, EmployeeResponse, EmployeeListResponse, EmployeeUpdate
-from .task_assignment import TaskDetailResponse, TaskStats, AssignmentEmployeeInfo, XodimTaskItem
--- backend/app/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .task_service import TaskService
-from .auth_service import AuthService
-from .employee_service import EmployeeService
-from .task_assignment_service import TaskAssignmentService
-from .report_service import ReportService
